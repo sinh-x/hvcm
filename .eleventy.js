016c11f7388316e95261a8b72a43944d30748658
Sinh Nguyen <sinh@sinhn.com>
2022-09-29 11:42:11 +0700

    Update header - navigation

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,20 @@
 const { DateTime } = require("luxon");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy('./src/style.css');
-    eleventyConfig.addPassthroughCopy('./src/assets');
-    eleventyConfig.addPassthroughCopy('./src/admin/config.yml')
-    
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
-    })
+  eleventyConfig.addPassthroughCopy('./src/style.css');
+  eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addPassthroughCopy('./src/admin/config.yml')
+  eleventyConfig.addPassthroughCopy('./src/js')
 
-    return {
-        dir: {
-            input: "src",
-            output: "public"
-        }
-    };
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  })
+
+  return {
+    dir: {
+      input: "src",
+      output: "public"
+    }
+  };
 }
