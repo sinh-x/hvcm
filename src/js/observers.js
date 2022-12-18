9b74b493e0c65366bf354e2c0a8aaa098588ec10
Sinh Nguyen <sinh@sinhn.com>
2022-12-19 06:31:55 +0700

    Update website layout & add lang-nghe-co-the

--- src/js/observers.js
+++ src/js/observers.js
@@ -1,10 +1,16 @@
 const header = document.querySelector("header");
 const nav = document.querySelector("nav");
 const sectionOne = document.querySelector(".home-intro");
+const sectionOneSmall = document.querySelector(".small-intro");
 
 const sectionOneOptions = {
   rootMargin: "-100px 0px 0px 0px"
 };
+
+const sectionTestOptions = {
+  rootMargin: "-500px 0px 0px 0px"
+};
+
 
 const sectionOneObserver = new IntersectionObserver(
   function (entries, sectionOneObserver) {
@@ -19,5 +25,18 @@ const sectionOneObserver = new IntersectionObserver(
     })
   }, sectionOneOptions)
 
+const sectionTestObserver = new IntersectionObserver(
+  function (entries, sectionOneObserver) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        header.classList.add("header-scrolled");
+        nav.classList.add("nav-scrolled")
+      } else {
+        header.classList.remove("header-scrolled");
+        nav.classList.remove("nav-scrolled")
+      }
+    })
+  }, sectionOneOptions)
 
 sectionOneObserver.observe(sectionOne)
+sectionOneObserver.observe(sectisectionOneSmallonOne)
